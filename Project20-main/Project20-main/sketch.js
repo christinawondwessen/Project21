d691b31fbee639d307065c9ee0cb66dfa3b8dd2e
christinawondwessen <78184033+christinawondwessen@users.noreply.github.com>
2021-09-19 14:37:38 -0400

    Add files via upload

---
+++ Project20-main/Project20-main/sketch.js
@@ -0,0 +1,62 @@
+var wall, thickness;
+var speed, weight, bullet;
+
+function setup() {
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  wall=createSprite(1200,200,thickness,height/2);
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+  wall=createSprite(1500,200,60,height/2);
+  bullet.velocityX = speed;
+}
+
+function draw() {
+  background(80,80,80);  
+  drawSprites();
+
+  if(wall.x-bullet.x<(bullet.width+wall.width/2)){
+    bullet.velocityX=0;
+    var deformation=0.5 * weight * speed * speed/22500;
+
+    if (deformation>180){
+      bullet.shapeColor=color(255,0,0);
+    }
+
+    if (deformation<180 && deformation>100){
+      bullet.shapeColor=color(230,230,0);
+    }
+
+    if (deformation<100){
+      bullet.shapeColor=color(0,255,0);
+    }
+
+  }
+
+}
+
+funtion hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x+lbullet.width
+
+  wallLeftEdge=lwall.x;
+
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+   }
+    return false;
+} 
+
+if (hasCollided(bullet,wall)){
+  bullet.velocityX=0;
+  var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+
+  if(damage>10){
+    wall.shapeColor=color(255,0,0);
+  }
+
+  if(damage<10){
+    wall.shapeColor=color(0,255,0);
+  }
+
+}
